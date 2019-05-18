e9e6782e325945fb2934d77f7f543f8fc1420fbb
Andy Tschida <atschida@persuadeloyalty.com>
2019-05-17 20:12:54 -0400

    Initial backend commit

---
+++ contact-manager/src/main/java/com/cop4331/group13/contactmanager/ContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cop4331.group13.contactmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerApplication.class, args);
+	}
+
+}
