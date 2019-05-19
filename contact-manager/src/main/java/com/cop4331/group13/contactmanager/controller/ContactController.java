1519f81350cd927eef4e6877ac9e7b6a8ee04ff5
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-05-18 23:09:05 -0400

    createContact passed basic test

--- contact-manager/src/main/java/com/cop4331/group13/contactmanager/controller/ContactController.java
+++ contact-manager/src/main/java/com/cop4331/group13/contactmanager/controller/ContactController.java
@@ -1,4 +1,21 @@
 package com.cop4331.group13.contactmanager.controller;
 
+import com.cop4331.group13.contactmanager.domain.Contact;
+import com.cop4331.group13.contactmanager.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ContactController {
+
+    @Autowired
+    private ContactService service;
+
+    @RequestMapping(value = "/contact/", method = RequestMethod.POST)
+    public Contact createContact(@RequestBody Contact contact) { return service.createContact(contact); }
+
+
 }
--- contact-manager/src/main/java/com/cop4331/group13/contactmanager/service/ContactService.java
+++ contact-manager/src/main/java/com/cop4331/group13/contactmanager/service/ContactService.java
@@ -1,4 +1,15 @@
 package com.cop4331.group13.contactmanager.service;
 
+import com.cop4331.group13.contactmanager.dao.ContactDao;
+import com.cop4331.group13.contactmanager.domain.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactService {
+
+    @Autowired
+    private ContactDao dao;
+
+    public Contact createContact(Contact contact) { return dao.save(contact); }
 }
