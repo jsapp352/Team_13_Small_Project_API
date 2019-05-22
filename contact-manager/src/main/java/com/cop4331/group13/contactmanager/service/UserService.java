a03cb6370820515f144337148f62c6456b73be3f
Andy Tschida <atschida@persuadeloyalty.com>
2019-05-21 20:42:57 -0400

    Complete User Endpoints V1

--- contact-manager/src/main/java/com/cop4331/group13/contactmanager/service/UserService.java
+++ contact-manager/src/main/java/com/cop4331/group13/contactmanager/service/UserService.java
@@ -3,6 +3,7 @@ package com.cop4331.group13.contactmanager.service;
 import com.cop4331.group13.contactmanager.dao.UserDao;
 import com.cop4331.group13.contactmanager.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,47 @@ public class UserService {
     }
 
     public User addUser(User user) {
+        try {
+            dao.save(user);
+        } catch (DataIntegrityViolationException e) {
+            return null;
+        }
+
+        return user;
+    }
+
+    public User getUserByUsername(String username) {
+        return dao.findByUsername(username);
+    }
+
+    public User loginUser(String username, String password) {
+        User user =  dao.findByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    public User updateUser(User user) {
         return dao.save(user);
+    }
+
+    public boolean deleteUser(long userId) {
+        try {
+            dao.deleteById(userId);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public User validateSecurityQuestion(String username, String securityAnswer) {
+        User user = dao.findByUsername(username);
+        if (user != null && user.getSecurityAnswer().equalsIgnoreCase(securityAnswer)) {
+            return user;
+        } else {
+            return null;
+        }
     }
 }
